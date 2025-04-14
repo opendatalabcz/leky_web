5affa82d460582f5f97f4a52fe776a112c6a5dc7
Jaroslav Machovec <jaroslav.machovec@applifting.cz>
2025-04-14 08:45:21 +0200

    123: Delete unused FE components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,20 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import {Navbar} from "./components/Navbar"
 import {HomePage} from "./pages/HomePage"
 import {AboutPage} from "./pages/AboutPage"
-
-import {CartProvider} from "./components/CartContext"
 import {UnifiedCartProvider} from "./components/UnifiedCartContext";
 
 export default function App() {
     return (
         <UnifiedCartProvider>
-            <CartProvider>
-                <BrowserRouter>
-                    <Navbar />
-                    <main style={{ padding: "2rem" }}>
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/about" element={<AboutPage />} />
-                        </Routes>
-                    </main>
-                </BrowserRouter>
-            </CartProvider>
+            <BrowserRouter>
+                <Navbar />
+                <main style={{ padding: "2rem" }}>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/about" element={<AboutPage />} />
+                    </Routes>
+                </main>
+            </BrowserRouter>
         </UnifiedCartProvider>
     )
 }
