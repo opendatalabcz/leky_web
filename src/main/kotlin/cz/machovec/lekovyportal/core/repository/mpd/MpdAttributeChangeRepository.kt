61e9d58796301926faec4866ee9827a1128eb23e
Jaroslav Machovec <jaroslav.machovec@applifting.cz>
2025-05-09 03:10:14 +0200

    207: Delete attribute changes entity and temporary absence entity

--- src/main/kotlin/cz/machovec/lekovyportal/core/repository/mpd/MpdAttributeChangeRepository.kt
+++
@@ -1,6 +0,0 @@
-package cz.machovec.lekovyportal.core.repository.mpd
-
-import cz.machovec.lekovyportal.core.domain.mpd.MpdAttributeChange
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MpdAttributeChangeRepository : JpaRepository<MpdAttributeChange, Long>
--- src/main/kotlin/cz/machovec/lekovyportal/core/repository/mpd/MpdRecordTemporaryAbsenceRepository.kt
+++
@@ -1,6 +0,0 @@
-package cz.machovec.lekovyportal.core.repository.mpd
-
-import cz.machovec.lekovyportal.core.domain.mpd.MpdRecordTemporaryAbsence
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MpdRecordTemporaryAbsenceRepository : JpaRepository<MpdRecordTemporaryAbsence, Long>
