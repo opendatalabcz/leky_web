404638bc52796a4ded4bf1fd020f2e13fb83eff1
Jaroslav Machovec <jaroslav.machovec@applifting.cz>
2025-12-15 15:27:42 +0100

    240: Fix footer to stick at the bottom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { EReceptPage } from "./pages/EReceptPage"
 import { FilterProvider } from "./components/FilterContext"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {SiteFooter} from "./components/SiteFooter";
+import {Box} from "@mui/material";
 
 const queryClient = new QueryClient()
 
@@ -16,16 +17,32 @@ export default function App() {
             <DrugCartProvider>
                 <FilterProvider>
                     <BrowserRouter>
-                        <Navbar />
-                        <main style={{ padding: "2rem" }}>
-                            <Routes>
-                                <Route path="/predepisovani-a-vydej" element={<EReceptPage />} />
-                                <Route path="/distribucni-tok" element={<DistributionPage />} />
-                                <Route path="/o-projektu" element={<AboutPage />} />
-                                <Route path="*" element={<Navigate to="/predepisovani-a-vydej" replace />} />
-                            </Routes>
-                        </main>
-                        <SiteFooter />
+                        <Box
+                            sx={{
+                                minHeight: "100vh",
+                                display: "flex",
+                                flexDirection: "column"
+                            }}
+                        >
+                            <Navbar />
+
+                            <Box
+                                component="main"
+                                sx={{
+                                    flex: 1,
+                                    padding: "2rem"
+                                }}
+                            >
+                                <Routes>
+                                    <Route path="/predepisovani-a-vydej" element={<EReceptPage />} />
+                                    <Route path="/distribucni-tok" element={<DistributionPage />} />
+                                    <Route path="/o-projektu" element={<AboutPage />} />
+                                    <Route path="*" element={<Navigate to="/predepisovani-a-vydej" replace />} />
+                                </Routes>
+                            </Box>
+
+                            <SiteFooter />
+                        </Box>
                     </BrowserRouter>
                 </FilterProvider>
             </DrugCartProvider>
