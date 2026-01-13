261d4f3b5a047c4a90f86360d8af73ec1189be11
Jaroslav Machovec <jaroslav.machovec@applifting.cz>
2026-01-13 21:45:30 +0100

    252: Fix 403 with CORS

--- src/main/kotlin/cz/machovec/lekovyportal/config/CorsConfig.kt
+++ src/main/kotlin/cz/machovec/lekovyportal/config/CorsConfig.kt
@@ -1,5 +1,6 @@
 package cz.machovec.lekovyportal.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
@@ -7,12 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class CorsConfig {
+
+    @Value("\${ALLOWED_ORIGINS:http://localhost:3000}")
+    private lateinit var allowedOrigins: String
+
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
+            val originsArray = allowedOrigins.split(",").toTypedArray()
+
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins(*originsArray)
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
             }
